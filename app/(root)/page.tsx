f9d6a18619d84ac76df920d862193212d287d8c6
AliAshour2 <aliashour468@gmail.com>
2024-07-25 18:09:28 +0300

    push

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    
+    <p>page</p>
+    </>
+    
+  )
+}
+
+export default Home
